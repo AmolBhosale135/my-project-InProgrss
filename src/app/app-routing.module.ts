cfae49261c83aa62bb94b26b464d4cf202b148dc
awesomearpit <jarpit1298@gmail.com>
2020-01-11 00:37:11 +0530

    my project working implements in progrss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,39 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from "./component/not-found/not-found.component";
+import { ContactComponent } from "./component/contact/contact.component";
+import { PortfolioComponent } from "./component/portfolio/portfolio.component";
+import { AboutUsComponent } from "./component/about-us/about-us.component";
+import { HomeComponent } from "./component/home/home.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+  },
+  {
+    path: "about-us",
+    component: AboutUsComponent,
+  },
+  {
+    path: "portfolio",
+    component: PortfolioComponent,
+  },
+  {
+    path: "contact",
+    component: ContactComponent,
+  },
+  { path: "404", component: NotFoundComponent },
+  { path: "**", redirectTo: "404" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { AboutUsComponent } from './component/about-us/about-us.component';
+import { PortfolioComponent } from './component/portfolio/portfolio.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { NotFoundComponent } from './component/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    PortfolioComponent,
+    ContactComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
